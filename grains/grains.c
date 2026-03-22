55e9bc84d513784e49281d34d2be8f622a6355f0
Dmytro Yakovenko <dmitry.yakovenko@gmail.com>
2026-03-22 13:13:30 +0100

    grains

---
+++ grains/grains.c
@@ -0,0 +1,18 @@
+#include "grains.h"
+
+uint64_t square(uint8_t index){
+    if(index < 1 || index > 64) return 0;
+    uint64_t result = 1;
+    for(uint8_t i = 2; i <= index; i++){
+        result *= 2;
+    }
+    return result;
+}
+
+uint64_t total(void){
+    uint64_t result = 0;
+    for(uint8_t i = 1; i <= 64; i++){
+        result += square(i);
+    }
+    return result;
+}
